da5274ea347674468e1bea44a6f236133de6a268
黒川颯 <ryu@kurokawasatsunoMacBook-Pro.local>
2025-02-14 21:37:24 +0900

    add src

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import {Route, BrowserRouter as Router,Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import Report from './pages/Report';
+import NoMatch from './pages/NoMatch';
+import AppLayout from './components/layout/AppLayout';
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+         <Route index element={<Home />} />
+         <Route path="/report" element={<Report />} />
+         <Route path="/*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
